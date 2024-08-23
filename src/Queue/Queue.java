645a151fc48a8ab3a932a23e283e89655734eee7
menukfernando <100038759+menukfernando@users.noreply.github.com>
2024-08-23 13:06:34 +0530

    update

--- src/Queue/Queue.java
+++ src/Queue/Queue.java
@@ -1,82 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Queue;
 
-/**
- *
- * @author Menuk Fernando
- */
-public class Queue {
-    private int maxValue;
+public class Queue{
+    private int maxSize;
     private int queueArray[];
     private int front;
     private int rear;
     private int numberOfElements;
     
-    public Queue(int maxValue){
-        this.maxValue = maxValue;
-        queueArray = new int[maxValue];
+    public Queue(int maxSize){
+        this.maxSize = maxSize;
+        queueArray = new int[maxSize];
         front = 0;
         rear = -1;
         numberOfElements = 0;
     }
     
     private void Insert(int value){
-        if(rear == maxValue-1){
-            System.out.println("Queue Array is Full");
+        if(rear == maxSize-1){
+            System.out.println("Queue Array is Full.");
         }else{
             queueArray[++rear] = value;
-            System.out.println("Value "+value+" added to the Queue Array");
+            numberOfElements++;
+            System.out.println("Value "+value+" has Inserted.");
         }
     }
     
     private void Remove(){
-        if(rear == -1){
-            System.out.println("Queue Array is Empty");
+        if(numberOfElements == 0){
+            System.out.println("Queue Array is Empty.");
         }else{
             numberOfElements--;
-            System.out.println("Value "+queueArray[front++]+" has Removed");
+            System.out.println("Value "+queueArray[front++]+" is Removed.");
         }
     }
     
     private void PeekFront(){
-        if(rear == -1){
+        if(numberOfElements == 0){
             System.out.println("Queue Array is Empty");
         }else{
-            System.out.println("Front Value is "+queueArray[front]+".");
+            System.out.println("Front value is "+queueArray[front]+".");
         }
     }
     
     private void PeekRear(){
-        if(rear == -1){
-            System.out.println("Queue Array is Empty");
+        if(numberOfElements == 0){
+            System.out.println("Queue Array is Empty.");
         }else{
-            System.out.println("Rear Value is "+queueArray[rear]);
+            System.out.println("Rear Value is "+queueArray[rear]+".");
         }
     }
     
     public static void main(String[] args) {
-        Queue queue = new Queue(3);
+        Queue queue = new Queue(5);
         
-        queue.Remove();
-        System.out.println("");
         
         queue.Insert(10);
-        queue.PeekFront();
-        System.out.println("");
-        
         queue.Insert(20);
+        queue.Insert(30);
+        queue.Insert(40);
+        queue.Insert(50);
+        
         System.out.println("");
         
-        queue.Insert(30);
+        queue.PeekFront();
+        queue.PeekRear();
+        
         System.out.println("");
         
         queue.Remove();
         queue.PeekFront();
-        queue.PeekRear();
+        System.out.println("");
+        
+        queue.Remove();
+        queue.PeekFront();
+        System.out.println("");
+        
+        queue.Remove();
+        queue.PeekFront();
+        System.out.println("");
+        
+        queue.Remove();
+        queue.PeekFront();
+        System.out.println("");
+        
+        queue.Remove();
+        queue.PeekFront();
         
     }
 }
